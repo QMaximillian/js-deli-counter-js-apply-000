f9c0dac38afbbff32ed63ce90be4a3e4fc3dab0f
Quinn Lashinsky <quinnlashinsky@wework.com>
2018-04-03 20:30:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 var katzDeliLine = [];
+var i = 0;
+//function takeANumber(katzDeliLine, person){
+	katzDeliLine.push(person);
 
-function takeANumber(katzDeliLine, person) {
- katzDeliLine.push(person);
- 
- return "Welcome, " + person + ". You are number " + katzDeliLine.length + " in line.";
+//	return "Welcome, " + person + ". You are number " + katzDeliLine.length + " in line.";
+//}
+
+function takeANumber(katzDeliLine){
+  i++;
+  katzDeliLine.push(i);
+  return "Welcome, you are number " + katzDeliLine[i] + " in line.";
+  
 }
 
-function nowServing(katzDeliLine) {
-   if (katzDeliLine.length === 0) {
-     return "There is nobody waiting to be served!";
-   } else {
-     return "Currently serving " + katzDeliLine.shift() + ".";
-    }
+function nowServing(katzDeliLine){
+	if (katzDeliLine.length === 0) {
+		return "There is nobody waiting to be served!";
+	} else {
+		return "Currently serving " + katzDeliLine.shift() + ".";
+	}
 }
 
-function currentLine(katzDeliLine) {
-   var line = [];
-   var i = 0;
-   while (i < katzDeliLine.length) {
-    line.push(` ` + [i+1]+ `. ` + katzDeliLine[i]);
-    i++;
-  } if (katzDeliLine.length === 0) {
-    return "The line is currently empty.";
-  } else {
-    return "The line is currently:" + line;
-  }
+function currentLine(katzDeliLine){
+	var line = [];
+	var i = 0;
+
+	while (i < katzDeliLine.length){
+		line.push(" " + [i+1] + ". " + katzDeliLine[i]);
+		i++;
+	}
+
+	if (katzDeliLine.length === 0) {
+		return "The line is currently empty.";
+	} else {
+		return "The line is currently:" + line;
+	}
 }
